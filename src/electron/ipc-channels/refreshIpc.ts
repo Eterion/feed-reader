adbfa4f7a0102ffc6e375f8049c2500b0c24c622
Eterion <Eterion@users.noreply.github.com>
2025-03-02 11:52:50 +0100

    Revert system notifications

--- src/electron/ipc-channels/refreshIpc.ts
+++ src/electron/ipc-channels/refreshIpc.ts
@@ -1,39 +1,11 @@
-import type { Article } from '@/types/Article';
 import type { Database } from '@/types/Database';
-import { isAfter, parseISO } from 'date-fns';
-import { Notification } from 'electron';
 import type { IpcChannel } from '../types/IpcChannel';
 import { IpcName } from '../types/IpcName';
 import { downloadFeedArticles } from '../utils/downloadFeedArticles';
-import { win } from '../win';
 
 export const refreshIpc: IpcChannel<[], Database> = {
   name: IpcName.Refresh,
   handler: async () => {
-    const db = await downloadFeedArticles();
-    showUnreadNotification(db);
-    return db;
+    return await downloadFeedArticles();
   },
 };
-
-function showUnreadNotification(db: Database) {
-  const now = new Date();
-  const getArticleDate = (articleData: Article['data']) =>
-    articleData.isoDate ? parseISO(articleData.isoDate) : now;
-  const unreadArticles = db.articles
-    .filter((article) => !article.isRead)
-    .map((article) => ({ ...article, isoDate: getArticleDate(article.data) }))
-    .sort((a, b) => (isAfter(a.isoDate, b.isoDate) ? -1 : 1))
-    .map((article) => article.data.title || 'Unknown Title');
-  if (unreadArticles.length) {
-    const notificaiton = new Notification({
-      title: `${unreadArticles.length} unread articles`,
-      body: unreadArticles.slice(0, 3).join(' | '),
-    });
-    notificaiton.show();
-    notificaiton.on('click', () => {
-      notificaiton.close();
-      win?.show();
-    });
-  }
-}
